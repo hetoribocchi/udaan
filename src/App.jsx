8ea4e13663d2c78997153fdd8fc25e2c2d0d4899
beezayrae <beezayrae@gmail.com>
2024-04-25 15:36:09 +0545

    Test

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Programs/>
+    </div>
+
+  )
+}
+
+export default App
