202a9e02ba1926fc1ab80db558fa28fdc3f9f9de
Nikesh-Lama <nikeshtmg555@gmail.com>
2024-04-27 13:32:39 +0545

    Titel & About

---
+++ src/components/About/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+const About = () => {
+    return (
+        <div className='about'>
+            <div className='about-left'>
+                <img src={about_img} alt="" className='about-img' />
+                <img src={play_icon} alt="" className='play-icon' />
+            </div>
+            <div className='about-right'>
+                <h3>About Universities</h3>
+                <h2>Nuturing Tommorow's Leaders Today</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit vero modi enim, rem dolor blanditiis mollitia sunt dolorum cupiditate corrupti quis corporis sequi ad dignissimos aut necessitatibus quod nobis obcaecati.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit vero modi enim, rem dolor blanditiis mollitia sunt dolorum cupiditate corrupti quis corporis sequi ad dignissimos aut necessitatibus quod nobis obcaecati.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit vero modi enim, rem dolor blanditiis mollitia sunt dolorum cupiditate corrupti quis corporis sequi ad dignissimos aut necessitatibus quod nobis obcaecati.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
