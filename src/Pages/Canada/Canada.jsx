d7a2440dfa3ab198644b8c98dbee577547ab53d8
hetoribocchi <nikeshtmg555@gmail.com>
2024-06-13 05:03:17 -0700

    canada css

--- src/Pages/Canada/Canada.jsx
+++ src/Pages/Canada/Canada.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import './Canada.css'
+import canada_background from '../../assets/Canada-background.jpg'
 
 const Canada = () => {
   return (
     <div>
-        <Navbar/>
-        <div></div>
+        <div><Navbar/></div>
+        <div className='background'>
+          <img src={canada_background} alt="" />
+          <div className='background-text'><h1>canada</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem delectus assumenda natus dolores tempore minus tenetur vitae quibusdam. Minus incidunt nemo quidem officia asperiores sed itaque alias ipsa nesciunt nihil.</p></div>
+        </div>
     </div>
   )
 }
